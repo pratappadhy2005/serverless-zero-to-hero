523804e7d65a1427e7157ab86914c773182b4901
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-21 21:59:32 +1100

    -Updated fucntion name

--- 01-greet-api/index.js
+++ 01-greet-api/index.js
@@ -1,9 +1,9 @@
-module.exports.handler = async (event) => {
+module.exports.greet = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
+        message: "Good day Pratap!",
         input: event,
       },
       null,
